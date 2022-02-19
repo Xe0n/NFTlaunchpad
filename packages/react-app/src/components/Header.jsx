147ed73cc773aa86501c00da2b81406e16aac52c
Xe0n <xeon.web@ya.ru>
2022-02-19 22:19:19 +0300

    add logo

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import { PageHeader } from "antd";
 import React from "react";
-
+import logo from "../assets/easygo.png"
 // displays a page header
 
 export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+      <img src={logo} alt="" className="logo"/>
       <PageHeader
-        title="LazyGo"
-        subTitle="Биржа для быстрого старта в мире NFT"
+        title=""
+        subTitle="Биржа для быстрого старта в мире NFT игр"
         style={{ cursor: "pointer" }}
       />
     </a>
